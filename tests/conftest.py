d6a3c5fc4daea0b8a24da7194ebf4f156906f6f0
Lee Zeman <lzeman@uvic.ca>
2026-01-13 17:22:12 -0800

    documentation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,17 @@
 import pytest
 import os
 
+# WARNING: in the test suite, paths are sometimes specified absolutely and sometimes relatively.
+# This seems to be required to make tests run correctly on github, but it's not entirely
+# clear why.
+# Changing relative paths to absolute paths in tests may cause tests to fail on github
+# (but not locally).
+# Specifically, test_check_range() fails in github's CI containers with absolute paths, but they're used in
+# other tests.
 
-# set environenment variables for testing - directory is test data directories, THREDDS is fake
+
+# set environenment variables for testing - directory is test data directories, THREDDS location
+# is a placeholder, checked against expected values but never accessed directly.
 @pytest.fixture(scope="session", autouse=True)
 def set_test_env_vars():
     os.environ["DATA_ROOT"] = os.path.abspath("tests/data/")
