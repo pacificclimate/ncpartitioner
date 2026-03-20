a5e0f88b6fe88e8c8f73881000688429a820d643
Lee Zeman <lzeman@uvic.ca>
2026-03-19 17:12:46 -0700

    hand-rolled chunking algorithm

--- ncpartitioner/response.py
+++ ncpartitioner/response.py
@@ -2,6 +2,7 @@
 In cases of DDS and DAS, the file already exists; for data requests the filemust be created first.
 """
 
+from math import ceil
 from posixpath import dirname
 import subprocess
 import os
@@ -14,34 +15,109 @@ logger = logging.getLogger(__name__)
 def slice(args):
     output_dir = os.getenv("OUTPUT_DIR")
     thredds_base = os.getenv("THREDDS_HTTP_BASE")
+    chunk_size = int(os.getenv("CHUNK_SIZE", "1000000000"))
 
-    logger.info(f"Slicing file")
-    try:
-        subprocess.run(
-            [
-                "ncks",
-                "--cnk_csh=1000000",
-                "-v",
-                f"{args['variable']}",
-                "-d",
-                f"time,{args['time'][0]},{args['time'][1]}",
-                "-d",
-                f"lat,{args['lat'][0]},{args['lat'][1]}",
-                "-d",
-                f"lon,{args['lon'][0]},{args['lon'][1]}",
-                f"/{args['dirname']}/{args['basename']}.{args['extension']}",
-                os.path.join(
-                    output_dir,
-                    f"{args['basename']}_{args['timestamp']}.{args['extension']}",
-                ),
-            ],
-            check=True,
+    # this is a home-grown and crude chunking algorithm.
+    # its purpose is to reduce the amount of RAM used by calls to ncks.
+    # ncks accepts arguments to configure runtime chunking, but it
+    # only seems to apply these to files as it writes them, not as it
+    # reads them, so the chunking arguments do not affect the RAM used
+    # by the ncks process itself.
+    #
+    # chunk size is set by the CHUNK_SIZE environment variable, and is measured
+    # in array cells (ignoring typing). Chunking is performed by slicing along
+    # the time axis, since for the datasets expected to be served by this service,
+    # time is the largest dimension.
+
+    num_chunks = ceil(
+        (
+            ((args["time"][1] - args["time"][0]) + 1)
+            * ((args["lat"][1] - args["lat"][0]) + 1)
+            * ((args["lon"][1] - args["lon"][0]) + 1)
         )
-    except subprocess.CalledProcessError as e:
-        logger.error(f"Error slicing file: {e}")
-        raise RuntimeError(f"Error slicing file: {e}")
-
+        / chunk_size
+    )
+    t_per_chunk = ceil((args["time"][1] - args["time"][0] + 1) / num_chunks)
     output_filename = f"{args['basename']}_{args['timestamp']}.{args['extension']}"
+
+    logger.info(
+        f"Slicing file into {num_chunks} subfiles of approximately {t_per_chunk} timesteps each"
+    )
+
+    # create component files with ncks
+    for i in range(num_chunks):
+        chunk_start = args["time"][0] + i * t_per_chunk
+        chunk_end = min(
+            args["time"][1],
+            args["time"][0] + (i + 1) * t_per_chunk - 1,
+        )
+
+        logger.info(
+            f"Slicing chunk {i + 1}/{num_chunks}: time[{chunk_start}:{chunk_end}]"
+        )
+
+        try:
+            subprocess.run(
+                [
+                    "ncks",
+                    "-v",
+                    f"{args['variable']}",
+                    "-d",
+                    f"time,{chunk_start},{chunk_end}",
+                    "-d",
+                    f"lat,{args['lat'][0]},{args['lat'][1]}",
+                    "-d",
+                    f"lon,{args['lon'][0]},{args['lon'][1]}",
+                    f"/{args['dirname']}/{args['basename']}.{args['extension']}",
+                    os.path.join(
+                        output_dir,
+                        f"{args['basename']}_chunk{i + 1}.{args['extension']}",
+                    ),
+                ],
+                check=True,
+            )
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Error slicing chunk {i + 1}: {e}")
+            raise RuntimeError(f"Error slicing chunk {i + 1}: {e}")
+
+    # merge component files with ncrcat (if needed)
+    if num_chunks > 1:
+        logger.info(f"Combining {num_chunks} subfiles into final output file")
+        try:
+            subprocess.run(
+                [
+                    "ncrcat",
+                    os.path.join(
+                        output_dir, f"{args['basename']}_chunk*.{args['extension']}"
+                    ),
+                    os.path.join(
+                        output_dir,
+                        output_filename,
+                    ),
+                ],
+                check=True,
+            )
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Error combining chunks: {e}")
+            raise RuntimeError(f"Error combining chunks: {e}")
+        finally:
+            # clean up chunk files
+            for i in range(num_chunks):
+                os.remove(
+                    os.path.join(
+                        output_dir,
+                        f"{args['basename']}_chunk{i + 1}.{args['extension']}",
+                    )
+                )
+    else:  # rename single chunk file
+        os.rename(
+            os.path.join(output_dir, f"{args['basename']}_chunk1.{args['extension']}"),
+            os.path.join(
+                output_dir,
+                output_filename,
+            ),
+        )
+
     logger.info(
         f"Slice complete; file saved to {os.path.join(output_dir, output_filename)}"
     )
